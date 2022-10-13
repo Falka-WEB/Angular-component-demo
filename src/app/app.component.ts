5dde085178c29661cbc8a1c0fb3c745ef379e39e
Vladyslav <vladislavshvetsua@gmail.com>
2022-10-13 23:31:42 +0200

    Added contactlist component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+export class PhoneBookRecord{
+  id: number = 0;
+  name: string = '';
+  surname: string = '';
+  phone: string = '';
+  email?: string = "";
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +15,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Contacts-Book';
+  PhoneBookRecords: PhoneBookRecord[] = [
+    {id:1, name:"Vlad", surname: "Surname", phone:"+380000000000", email:"vladislav@example.com"},
+    {id:2, name:"Anna", surname: "AnotherSurname", phone:"+380000000001", email:"anna@example.com"},
+    {id:3, name:"ななみ", surname: "SomeSurname", phone:"+380000000002", email:"ななみ@example.com"},
+  ];
 }
